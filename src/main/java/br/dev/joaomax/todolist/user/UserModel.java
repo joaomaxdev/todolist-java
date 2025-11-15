4b00f613318d70f44de3f2455c05be8558a70f03
joaomaxdev <r15.joaomarques@gmail.com>
2025-11-15 16:25:37 -0300

    feat(user): add UserRepository with JPA and return ResponseEntity in user creation

--- src/main/java/br/dev/joaomax/todolist/user/UserModel.java
+++ src/main/java/br/dev/joaomax/todolist/user/UserModel.java
@@ -1,9 +1,6 @@
 package br.dev.joaomax.todolist.user;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -18,6 +15,7 @@ public class UserModel {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Column(unique = true)
     private String username;
     private String name;
     private String password;
