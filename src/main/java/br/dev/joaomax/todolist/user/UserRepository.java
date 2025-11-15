366f83fa8f3f601188df31041eefc73e63742740
joaomaxdev <r15.joaomarques@gmail.com>
2025-11-15 15:47:47 -0300

    first commit

---
+++ src/main/java/br/dev/joaomax/todolist/user/UserRepository.java
@@ -0,0 +1,9 @@
+package br.dev.joaomax.todolist.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+}
