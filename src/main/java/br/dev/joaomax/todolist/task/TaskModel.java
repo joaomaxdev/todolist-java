3e69aea52ff937804a8c3bffc7b15bba8bb4707b
joaomaxdev <r15.joaomarques@gmail.com>
2025-11-15 16:55:19 -0300

    feat(task): add TaskModel for todo list domain

---
+++ src/main/java/br/dev/joaomax/todolist/task/TaskModel.java
@@ -0,0 +1,29 @@
+package br.dev.joaomax.todolist.task;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name = "tb_tasks")
+public class TaskModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String description;
+
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+
+    private UUID idUser;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
