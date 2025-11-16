f52df57ad480368c7f6878431eaf651115dbb3ed
joaomaxdev <r15.joaomarques@gmail.com>
2025-11-16 14:12:51 -0300

    feat(auth): add FilterTaskAuth and integrate authentication into TaskController

---
+++ src/main/java/br/dev/joaomax/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,16 @@
+package br.dev.joaomax.todolist.filter;
+
+import jakarta.servlet.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class FilterTaskAuth implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        System.out.println("Chegou no filtro");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
