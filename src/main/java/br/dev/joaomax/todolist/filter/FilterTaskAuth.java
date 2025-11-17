2027ed1f2b280c51f04ced8fb82dc060553eedf6
joaomaxdev <r15.joaomarques@gmail.com>
2025-11-17 00:49:13 -0300

    feat(auth): validate user credentials using Basic Auth in OncePerRequestFilter

--- src/main/java/br/dev/joaomax/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/dev/joaomax/todolist/filter/FilterTaskAuth.java
@@ -1,16 +1,38 @@
 package br.dev.joaomax.todolist.filter;
 
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Base64;
 
 @Component
-public class FilterTaskAuth implements Filter {
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+public class FilterTaskAuth extends OncePerRequestFilter {
 
-        System.out.println("Chegou no filtro");
-        filterChain.doFilter(servletRequest, servletResponse);
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        //Pegar autenticação (usuario e senha)
+        var authorization = request.getHeader("Authorization");
+        var authEncoded = authorization.substring("Basic".length()).trim();
+
+        byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+
+        var authString = new String(authDecode);
+
+        String[] credentials = authString.split(":");
+        String username = credentials[0];
+        String password = credentials[1];
+        System.out.println(username + password);
+
+        //Validar usuario
+
+        //Validar senha
+
+        filterChain.doFilter(request, response);
+
     }
 }
